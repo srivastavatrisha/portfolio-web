5cb10614792e5d1e2263c2a931393cf67ec8a9d2
Trisha srivastava <123794254+srivastavatrisha@users.noreply.github.com>
2024-09-13 19:44:17 +0530

    added to style  and js

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text" , {
+    strings: ["Web Developer", "Student", 
+    "Learner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
